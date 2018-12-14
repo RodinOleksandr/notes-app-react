c726c43eb63529f9b7ea30500e167ce1027ada2d
unknown <rodinalexandr1@gmail.com>
2018-12-14 15:21:54 +0200

    Initial commit

---
+++ precache-manifest.ee350492bb29da6e478469616f9035ce.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "743bc3fe91850ebde470",
+    "url": "/static/js/main.743bc3fe.chunk.js"
+  },
+  {
+    "revision": "2158cd33bc0e7055c097",
+    "url": "/static/js/1.2158cd33.chunk.js"
+  },
+  {
+    "revision": "743bc3fe91850ebde470",
+    "url": "/static/css/main.f569ebfd.chunk.css"
+  },
+  {
+    "revision": "163b6dff3117d696a49e876961b45d8b",
+    "url": "/index.html"
+  }
+];
